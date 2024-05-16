cca97b72e96e14997f0cd4644e4764ca8e29a75e
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-05-16 21:12:40 +0700

    added results

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Input from "./components/Input";
+import Results from "./components/Results";
 
 function App() {
   
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Input/>
+      <Results />
     </>
   );
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,26 @@
+export default function Results() {
+  return (
+    <section>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>sadasdasd</td>
+            <td>sadasdasd</td>
+            <td>sadasdasd</td>
+            <td>sadasdasd</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
+}
